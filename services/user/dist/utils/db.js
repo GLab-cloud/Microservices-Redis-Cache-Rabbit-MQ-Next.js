5063b24b247a13e4a20f3474bc24408e7b591a8b
dangtrantrung <trung280584@gmail.com>
2026-07-05 12:23:07 +0700

    Connect and Activate MongoDB

---
+++ services/user/dist/utils/db.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+const connectDb = async () => {
+    if (!process.env.MONGO_URI) {
+        console.error('Error: MONGO_URI undefined in file .env!');
+        process.exit(1);
+    }
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to Mongo DB');
+        connectAndActivate();
+        checkDatabaseExists();
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+async function checkDatabaseExists() {
+    try {
+        const adminDb = mongoose.connection.db.admin();
+        const dbsList = await adminDb.listDatabases();
+        const dbNames = dbsList.databases.map((db) => db.name);
+        const isExist = dbNames.includes('BlogApp');
+        console.log('--- DATABASE ON CLUSTER ---');
+        console.log(dbNames);
+        console.log('----------------------------------------');
+        if (isExist) {
+            console.log("Database 'BlogApp' already exist on Cluster!");
+        }
+        else {
+            console.log("Database 'BlogApp' does not exist on the cluster (or it is empty).");
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+const TestModel = mongoose.model('test_collection', new mongoose.Schema({ name: String }));
+async function connectAndActivate() {
+    try {
+        await TestModel.create({ name: 'activate database BlogApp' });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+export default connectDb;
--- services/user/src/server.ts
+++ services/user/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './utils/db.js'
+dotenv.config()
 const app = express()
+connectDb()
+console.log(process.env.MONGO_URI)
+const port = process.env.PORT
 app.listen(5000, () => {
-  console.log(`Server is running on http://localhost:5000`)
+  console.log(`Server is running on http://localhost:${port}`)
 })
