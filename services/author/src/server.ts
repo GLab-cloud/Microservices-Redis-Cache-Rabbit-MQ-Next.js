5603b795df12f173c0247026c1c37fd3ae29055e
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-07-19 05:28:06 +0000

    Neon database initialized successfully

--- services/author/src/server.ts
+++ services/author/src/server.ts
@@ -1,10 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./utils/db.js";
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+async function initDB(){
+  try{
+    await sql `CREATE TABLE IF NOT EXISTS blogs (
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL,    
+    description VARCHAR(255) NOT NULL,
+    blogcontent TEXT NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+     await sql `CREATE TABLE IF NOT EXISTS comments (
+    id SERIAL PRIMARY KEY, 
+    comment TEXT NOT NULL,    
+    userid VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    blogid VARCHAR(255) NOT NULL,    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+     await sql `CREATE TABLE IF NOT EXISTS savedblogs (
+    id SERIAL PRIMARY KEY, 
+    userid VARCHAR(255) NOT NULL,
+    blogid VARCHAR(255) NOT NULL,    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+    console.log("database initialized successfully");
+    const tables = await sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`;
+    console.log("Tables in database:", tables.map(t => t.table_name).join(", "));
+  }
+  catch(error){
+    console.error("Error initializing database:", error);
+  }
+}
+
+initDB().then(()=>{
+    app.listen(port, () => {  console.log(`Server is running on http://localhost:${port}`);
+})});
