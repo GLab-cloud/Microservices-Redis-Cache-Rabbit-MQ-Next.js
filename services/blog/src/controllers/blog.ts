279f16297d07a667c7fe95dde6c404481dc99be6
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-08-02 07:09:06 +0000

    feat(blog): improve caching logic for blog fetching; enhance logging and response structure

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -4,11 +4,11 @@ import TryCatch from "../utils/trycatch.js";
 import axios from "axios";
 
 export const getAllBlogs = TryCatch(async (req, res) => {
-    const {searchQuery="",category=""} = req.query;
+    const {searchQuery = "", category = ""} = req.query;
     const cacheKey = `blogs:${searchQuery}:${category}`;
     const cachedBlogs = await redisClient.get(cacheKey);
     if(cachedBlogs){
-        console.log("Blogs fetched from  Redis cache");
+        console.log("Blogs fetched from Redis cache");
         return res.status(200).json(JSON.parse(cachedBlogs));
     }
     let blogs 
@@ -28,12 +28,20 @@ export const getAllBlogs = TryCatch(async (req, res) => {
 })
 export const getBlogById = TryCatch(async (req, res) => {
     const { id } = req.params;
+    const cacheKey = `blog:${id}`;
+    const cachedBlog = await redisClient.get(cacheKey);
+    if(cachedBlog){
+        console.log("Blog fetched from Redis cache");
+        return res.status(200).json(JSON.parse(cachedBlog));
+    }
     const blog = await sql`SELECT * FROM blogs WHERE id = ${id}`;
     if (blog.length === 0) {
         res.status(404).json({ message: "Blog not found" });
         return;
     }
     const{data}=await axios.get(`${process.env.USER_SERVICE_URL}/api/v1/user/${blog[0].author}`);
-    res.status(200).json({blog:blog[0], author: data});
-    console.log("Blog fetched successfully", {blog:blog[0], author: data});
+    const responseData = {blog:blog[0], author: data};
+    await redisClient.set(cacheKey, JSON.stringify(responseData), {EX: 3600}); // Cache for 1 hour
+    console.log("Blog fetched successfully from database", responseData);
+    return res.status(200).json(responseData);  
 })
