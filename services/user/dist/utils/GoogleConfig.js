065c88010346ab447e9b129d06bba2dbded1ae70
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-08-21 01:52:19 +0000

    feat: add Radix UI - Shadcn/ui and implement new button component - Added Radix UI-Shadcn/ui as a dependency in package.json. - Updated globals.css to include new CSS variables and dark mode support. - Refactored the Home page to use a new Button component. - Implemented Google OAuth login functionality in user controller. - Created Google configuration utility for OAuth client. - Added a new Login page component. - Created a Navbar component for future use. - Introduced a utility function for class name merging. - Added a components.json file for component configuration.

--- services/user/dist/utils/GoogleConfig.js
+++ services/user/dist/utils/GoogleConfig.js
@@ -1,6 +1,6 @@
 import { google } from 'googleapis';
 import dotenv from 'dotenv';
 dotenv.config();
-const GoogleConfig = () => {
-    const oauth2Client = new google.auth.OAuth2(process.env.Google_Client_ID, process.env.Google_Client_Secret, 'http://localhost:3000/auth/google/callback');
-};
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "postmessage");
